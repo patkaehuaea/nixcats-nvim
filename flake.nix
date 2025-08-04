ad5b8174f544859c948320790cfa37c2c7258229
patkaehuaea <2474609+patkaehuaea@users.noreply.github.com>
2025-08-04 13:26:15 -0700

    remove zellij splits

--- flake.nix
+++ flake.nix
@@ -146,7 +146,6 @@
               snacks-nvim
               nvim-treesitter-textobjects
               nvim-treesitter.withAllGrammars
-              zellij-nav-nvim
               # This is for if you only want some of the grammars
               # (nvim-treesitter.withPlugins (
               #   plugins: with plugins; [
